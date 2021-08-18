854866e67d516f261fb626f073bd7e0b5e485999
Rafal <rafalswiech3@gmail.com>
2021-08-18 10:11:06 +0200

    Added BMRCalculatorUtil with functions without implementation

---
+++ app/src/main/java/com/rafal/caloriecounter/utilities/BMRCalculatorUtil.kt
@@ -0,0 +1,38 @@
+package com.rafal.caloriecounter.utilities
+
+class BMRCalculatorUtil {
+    fun validateGender(input: String): Boolean {
+        return false
+    }
+
+    fun validateWeight(input: String): Boolean {
+        return false
+    }
+
+    fun validateHeight(input: String): Boolean {
+        return false
+    }
+
+    fun validateAge(input: String): Boolean {
+        return false
+    }
+
+    fun validateActivity(input: String): Boolean {
+        return false
+    }
+
+    fun validateGoal(input: String): Boolean {
+        return false
+    }
+
+    fun calculateBMR(
+        gender: String,
+        weight: Float,
+        height: Float,
+        age: Int,
+        activity: String,
+        goal: String
+    ): Int {
+        return 0
+    }
+}
